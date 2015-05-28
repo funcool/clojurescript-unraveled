e87b549f5532140494bf22045e85bfe66d4c6e1a
Andrey Antukh <niwi@niwi.nz>
2015-05-28 16:35:10 +0200

    Add nashorn repl playground.

--- playground/repl.clj
+++
@@ -1,7 +0,0 @@
-(ns init-repl-test
-  (:require [cljs.repl]
-            [cljs.repl.nashorn]))
-
-(cljs.repl/repl (cljs.repl.nashorn/repl-env)
-                :output-dir "out"
-                :cache-analysis true)
