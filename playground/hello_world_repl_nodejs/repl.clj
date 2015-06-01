99fcdd54f107fd0a317d17cd98d2f89ff1378d0e
Andrey Antukh <niwi@niwi.nz>
2015-06-01 23:17:53 +0200

    Add nodejs repl section.

--- playground/hello_world_repl_nodejs/repl.clj
+++ playground/hello_world_repl_nodejs/repl.clj
@@ -1,7 +1,7 @@
 (require '[cljs.repl]
-         '[cljs.repl.nashorn])
+         '[cljs.repl.node])
 
 (cljs.repl/repl
- (cljs.repl.nashorn/repl-env)
+ (cljs.repl.node/repl-env)
  :output-dir "out"
  :cache-analysis true)
