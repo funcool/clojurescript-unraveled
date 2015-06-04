4475ec006b4224bcae24b68345f04e60a6bc91b1
Alejandro Gómez <alejandro@dialelo.com>
2015-06-04 11:53:15 +0200

    Use node REPL by default

--- playground/repl.sh
+++ playground/repl.sh
@@ -1,4 +1,4 @@
 if [ ! -e cljs.jar ]; then
     wget https://github.com/clojure/clojurescript/releases/download/r3211/cljs.jar
 fi
-rlwrap java -jar cljs.jar repl.clj
+rlwrap java -jar cljs.jar hello_world_repl_nodejs/repl.clj
