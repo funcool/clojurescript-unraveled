360859aafaf7be587958d95cef5678470cf15aed
Andrey Antukh <niwi@niwi.nz>
2015-07-13 11:12:53 +0300

    Update to the last clojurescript compiler (3308).

--- playground/initialize.sh
+++ playground/initialize.sh
@@ -4,4 +4,4 @@ if [ -r cljs.jar ]; then
     rm cljs.jar;
 fi
 
-wget https://github.com/clojure/clojurescript/releases/download/r3269/cljs.jar
+wget https://github.com/clojure/clojurescript/releases/download/r3308/cljs.jar
